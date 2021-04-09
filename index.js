83df89b7aba9845388c15eeae6e3dfbe0af5797f
Jeffrey <renjeffreyy@gmail.com>
2021-04-08 20:57:37 -0700

    finished front end challenge

--- index.js
+++ index.js
@@ -1,32 +1,76 @@
-const couraselDataDesktop = [
-  {
-    heading: 'Discover innovative ways to decorate',
-    paragraph:
-      'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
-    img: './images/mobile-image-hero-1.jpg',
-  },
-  {
-    heading: 'We are available all across the globe',
-    paragraph:
-      'With stores all over the world, it&lsquo;s easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don&lsquo;t hesitate to contact us today.',
-    img: './images/mobile-image-hero-2.jpg',
-  },
-  {
-    heading: 'Manufactured with the best materials',
-    paragraph:
-      'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
-    img: './images/mobile-image-hero-3.jpg',
-  },
-];
+let currentSlide = 0; //counter for slides
+let heroImgContainer = document.querySelectorAll('.hero-img'); // images for hero slideshow
+let heroTextContainer = document.querySelectorAll('.hero-text'); // descriptions for each hero slide
+let navControl = document.querySelectorAll('.nav-menu-icon'); //icon control for mobile nav
+let displayMobileNav = false; // display mobile navigation menu boolean
 
-function handleBurgerClick() {
-  console.log('hamburger opened');
+//if current slide is last slide then start back from beginning
+function incramentSlide() {
+  currentSlide < 2 ? currentSlide++ : (currentSlide = 0);
+  setActiveSlide();
 }
 
-function couraselClickRight() {
-  console.log('right');
+// if slide is 0 then go to last slide in order
+function decramentSlide() {
+  currentSlide > 0 ? currentSlide-- : (currentSlide = 2);
+  setActiveSlide();
 }
 
-function couraselClickLeft() {
-  console.log('left');
+//sets the active class to next item in slideshow and removes active class from non0active slides
+function setActiveSlide() {
+  heroImgContainer.forEach((slide) => {
+    slide.classList.remove('active');
+    heroImgContainer[currentSlide].classList.add('active');
+  });
+
+  heroTextContainer.forEach((slide) => {
+    slide.classList.remove('active');
+    heroTextContainer[currentSlide].classList.add('active');
+  });
 }
+
+//toggle mobile navbar display when user clicks hamgurger menu / X to close
+function toggleMobileNavDisplay() {
+  let navbarSelector = document.getElementsByClassName('nav-container');
+  let navLogo = document.getElementsByClassName('nav-logo');
+  let navLinksContainer = document.getElementsByClassName('nav-link-container');
+  let placeholderBurger = document.getElementsByClassName('nav-menu-burger');
+  let navOverlay = document.getElementsByClassName('nav-overlay');
+
+  // toggles wehter to display mobile nav options
+  displayMobileNav = !displayMobileNav;
+
+  if (displayMobileNav) {
+    navLogo[0].classList.add('display-none');
+    navbarSelector[0].classList.add('active');
+    navControl[1].classList.remove('display-none');
+    navControl[0].classList.add('display-none');
+    navLinksContainer[0].classList.add('active');
+    placeholderBurger[0].classList.add('display-none');
+    navOverlay[0].classList.add('active');
+  } else {
+    navLogo[0].classList.remove('display-none');
+    navbarSelector[0].classList.remove('active');
+    navControl[1].classList.add('display-none');
+    navControl[0].classList.remove('display-none');
+    navLinksContainer[0].classList.remove('active');
+    placeholderBurger[0].classList.remove('display-none');
+    navOverlay[0].classList.remove('active');
+  }
+}
+
+// after 7 seconds change to next slide for slideshow effect
+setTimeout(() => {
+  window.setInterval(() => {
+    incramentSlide();
+  }, 7000);
+}, 7000);
+
+//listen for key down events on left and right arrow keys to control slideshow
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'ArrowLeft') {
+    decramentSlide();
+  } else if (e.key == 'ArrowRight') {
+    incramentSlide();
+  }
+});
--- reflection.md
+++
@@ -1,4 +0,0 @@
-### Ny First Frontend Coding Challenge
-
-## initial thoughts
-  This looks simple enough but there are probably going to be a lot of things i have to look up to refresh my memore or learn. I've mostly only built front end projects using frameworks like React, Angular, and Bootstrap. 
